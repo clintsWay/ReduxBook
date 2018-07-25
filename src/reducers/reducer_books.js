13fb805f4941c09feda6503eeb0d3b4c3860bbed
Clint Ast <clintast@gmail.com>
2018-07-25 09:25:32 -0500

    adding actions and reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: 'Javascript'},
+        { title: 'Harry Potter' },
+        { title: 'The Dark Tower'},
+        { title: 'My pants are on fire' },
+        { title: 'Star Trek XX'}
+    ]
+}
